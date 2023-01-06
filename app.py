90cb0193d6830de24243193c49dbbbf8278ba8d8
Zeed <88276750+ZeEeEeEed29@users.noreply.github.com>
2023-01-06 23:24:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-from app import app
+#from app import app
+from flask import Flask
+
+app = Flask(__name__)
+
+app.config.from_object("config.DevelopmentConfig")
+
 
 if __name__ == '__main__':
    app.run()
